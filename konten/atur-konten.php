e64aa0546bce120da65d85c4232919ed07679555
Hilman F.N <93692911+HILMANFAUZAN@users.noreply.github.com>
2022-01-27 11:23:52 +0700

    Add files via upload

--- konten/atur-konten.php
+++ konten/atur-konten.php
@@ -2,3 +2,8 @@
 if ($_GET['menu'] == 'home'){
     include("konten/home.php");
 }
+else if ($_GET['menu'] == 'logout'){
+    session_start();
+    session_destroy();
+    print "<meta http-equip="refresh" content=0; url=?menu=login'>";
+}
