c9aad42918bd115caf1a1c94d7c016ea6aaa854d
Hilman F.N <93692911+HILMANFAUZAN@users.noreply.github.com>
2022-02-10 09:34:58 +0700

    Delete konten directory

--- konten/atur-konten.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_GET['menu'] == 'home'){
-    include("konten/home.php");
-}
-else if ($_GET['menu'] == 'logout'){
-    session_start();
-    session_destroy();
-    print "<meta http-equip="refresh" content=0; url=?menu=login'>";
-}
